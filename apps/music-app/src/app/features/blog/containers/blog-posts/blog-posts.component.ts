7792649b2860124308d31593af27d02759291c15
Your Name <you@example.com>
2020-03-09 14:23:42 +0100

    ngIf ngIfThen ngIfElse, ng-template, # - localREf

--- apps/music-app/src/app/features/blog/containers/blog-posts/blog-posts.component.ts
+++ apps/music-app/src/app/features/blog/containers/blog-posts/blog-posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { BlogApiService } from 'apps/music-app/src/app/core/api/blog-api.service';
+import { Post } from 'apps/music-app/src/app/core/models/Post';
 
 @Component({
   selector: 'music-apps-blog-posts',
@@ -7,11 +8,18 @@ import { BlogApiService } from 'apps/music-app/src/app/core/api/blog-api.service
   styleUrls: ['./blog-posts.component.css']
 })
 export class BlogPostsComponent implements OnInit {
+  posts: Post[] = [];
 
   constructor(
     // @Inject(BlogApiServiceToken)
     private service: BlogApiService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    this.service.fetchRecentPosts()
+                .subscribe(posts => {
+                  this.posts = posts;
+                });
+  }
 }
