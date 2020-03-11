cb9206acb888070184ac65dbf6cef267f2ba062d
Your Name <you@example.com>
2020-03-11 12:09:13 +0100

    mergeMap, concatMap, switchMap, exhaustMap

--- apps/music-app/src/app/core/services/music-search.service.ts
+++ apps/music-app/src/app/core/services/music-search.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { AlbumsResponse } from '../models/music/AlbumsResponse';
 import { AuthService } from '../../security/services/auth.service';
-import { map, catchError, switchMapTo, startWith } from 'rxjs/operators';
+import { map, catchError, switchMapTo, startWith, mergeAll, mergeMap, concatAll, exhaust, switchAll, concatMap, switchMap, exhaustMap } from 'rxjs/operators';
 import {
   throwError,
   EMPTY,
@@ -13,7 +13,8 @@ import {
   merge,
   concat,
   ReplaySubject,
-  BehaviorSubject
+  BehaviorSubject,
+  Observable
 } from 'rxjs';
 import { Album } from '../models/music/Album';
 
@@ -43,34 +44,47 @@ export class MusicSearchService {
     }
   ]);
   private queryChanges = new BehaviorSubject<string>('batman');
-  public queries = this.queryChanges.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    const o: Observable<Album[]> = this.queryChanges
+      .pipe(
+        switchMap(query => this.requestAlbums(query)),
+        // mergeMap(query => this.requestAlbums(query)),
+        // concatMap(query => this.requestAlbums(query)),
+        // exhaustMap(query => this.requestAlbums(query)),
+        // mergeAll(),
+        // concatAll(),
+        // switchAll(),
+        // exhaust(),
+        o => o,
+        map(res => res.albums.items)
+      )
+
+
+      // .subscribe({
+      //   next: albums => this.albumChanges.next(albums),
+      //   error: error => this.errorNotifications.next(error)
+      // });
+  }
 
   api_url = 'https://api.spotify.com/v1/search';
 
+  private requestAlbums(query: string) {
+    return this.http.get<AlbumsResponse>(this.api_url, {
+      params: {
+        query: query,
+        type: 'album'
+      } as SearchParams
+    });
+  }
+
+  // Inputs
   search(query: string) {
     this.queryChanges.next(query);
-
-    this.requestAlbums(query)
-      .pipe(map(res => res.albums.items))
-      .subscribe({
-        next: albums => this.albumChanges.next(albums),
-        error: error => this.errorNotifications.next(error)
-      });
   }
 
-  albums: Album[] = [];
-
-  private requestAlbums(query: string) {
-    return this.http
-      .get<AlbumsResponse>(this.api_url, {
-        params: {
-          query: query,
-          type: 'album'
-        } as SearchParams
-      });
-  }
+  // Outputs
+  public queries = this.queryChanges.asObservable();
 
   getAlbums() {
     return this.albumChanges;
