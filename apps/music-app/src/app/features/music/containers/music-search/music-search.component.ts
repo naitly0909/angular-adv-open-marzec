c15b53c6d4371f6e19f4b2eab561f4f14077e678
Your Name <you@example.com>
2020-03-11 12:44:51 +0100

    async pipe resubscribe

--- apps/music-app/src/app/features/music/containers/music-search/music-search.component.ts
+++ apps/music-app/src/app/features/music/containers/music-search/music-search.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../../../../core/models/music/Album';
 import { MusicSearchService } from 'apps/music-app/src/app/core/services/music-search.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { tap, takeUntil, multicast, refCount, share, shareReplay } from 'rxjs/operators';
+import {
+  tap,
+  takeUntil,
+  multicast,
+  refCount,
+  share,
+  shareReplay
+} from 'rxjs/operators';
 import {
   Subscription,
   Subject,
@@ -19,20 +26,14 @@ export class MusicSearchComponent implements OnInit {
   query$ = this.service.queries;
   message = '';
 
-  // results$ = this.service.getAlbums().pipe(multicast(() => new ReplaySubject(1)));
-  results$ = this.service.getAlbums().pipe(
-    // multicast(() => new ReplaySubject(1)),
-    // refCount()
-    // share()
-    shareReplay()
-  );
+  results$ = this.service.getAlbums().asObservable() //.pipe(shareReplay());
 
   constructor(private service: MusicSearchService) {}
 
   ngOnInit(): void {}
 
   connect() {
-    // (this.results$ as ConnectableObservable<Album[]>).connect();
+    this.results$ = this.service.getAlbums().pipe(tap(console.log)) //.pipe(shareReplay());
   }
 
   search(query: string) {
