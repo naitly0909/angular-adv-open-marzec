43b5801736de7f2a65660c6868af644524fb457d
Your Name <you@example.com>
2020-03-11 12:23:21 +0100

    takeUntil - unsubscribe

--- apps/music-app/src/app/features/music/containers/music-search/music-search.component.ts
+++ apps/music-app/src/app/features/music/containers/music-search/music-search.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../../../../core/models/music/Album';
 import { MusicSearchService } from 'apps/music-app/src/app/core/services/music-search.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { tap, takeUntil } from 'rxjs/operators';
+import { Subscription, Subject } from 'rxjs';
 
 @Component({
   selector: 'music-apps-music-search',
@@ -18,27 +18,28 @@ export class MusicSearchComponent implements OnInit {
   constructor(private service: MusicSearchService) {}
 
   ngOnInit(): void {
-    this.sub.add(
-      this.service.queries.subscribe(query => {
+    this.service.queries
+      .pipe(takeUntil(this.destroySignal))
+      .subscribe(query => {
         this.query = query;
-      })
-    );
+      });
 
-    this.sub.add(
-      this.service.getAlbums().subscribe({
+    this.service
+      .getAlbums()
+      .pipe(takeUntil(this.destroySignal))
+      .subscribe({
         next: results => (this.results = results),
         error: error => (this.message = error.message)
-      })
-    );
+      });
   }
 
   search(query: string) {
     this.service.search(query);
   }
 
-  sub = new Subscription();
+  destroySignal = new Subject();
 
   ngOnDestroy() {
-    this.sub.unsubscribe();
+    this.destroySignal.next();
   }
 }
