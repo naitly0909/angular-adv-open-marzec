230c21de0c2e73a96774fafa1fad2ded6c2e20d1
Your Name <you@example.com>
2020-03-11 12:39:51 +0100

    share, shareReplay

--- apps/music-app/src/app/features/music/containers/music-search/music-search.component.ts
+++ apps/music-app/src/app/features/music/containers/music-search/music-search.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../../../../core/models/music/Album';
 import { MusicSearchService } from 'apps/music-app/src/app/core/services/music-search.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { tap, takeUntil } from 'rxjs/operators';
-import { Subscription, Subject } from 'rxjs';
+import { tap, takeUntil, multicast, refCount, share, shareReplay } from 'rxjs/operators';
+import {
+  Subscription,
+  Subject,
+  ConnectableObservable,
+  ReplaySubject
+} from 'rxjs';
 
 @Component({
   selector: 'music-apps-music-search',
@@ -11,13 +16,24 @@ import { Subscription, Subject } from 'rxjs';
   styleUrls: ['./music-search.component.scss']
 })
 export class MusicSearchComponent implements OnInit {
-  results$ = this.service.getAlbums();
   query$ = this.service.queries;
   message = '';
+
+  // results$ = this.service.getAlbums().pipe(multicast(() => new ReplaySubject(1)));
+  results$ = this.service.getAlbums().pipe(
+    // multicast(() => new ReplaySubject(1)),
+    // refCount()
+    // share()
+    shareReplay()
+  );
 
   constructor(private service: MusicSearchService) {}
 
   ngOnInit(): void {}
+
+  connect() {
+    // (this.results$ as ConnectableObservable<Album[]>).connect();
+  }
 
   search(query: string) {
     this.service.search(query);
